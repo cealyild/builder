5daf136de2670cb7c320305f5b3d30b754bd5c0b
Celal Yıldırım <yildirimcelal1997@gmail.com>
2023-07-04 02:18:06 +0300

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,49 @@
+import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
+import { useEffect, useState } from "react";
+
+builder.init("YOUR_URL");
+
+function App() {
+  const isPreviewingInBuilder = useIsPreviewing();
+  const [notFound, setNotFound] = useState(false);
+  const [content, setContent] = useState(null);
+
+  // get the page content from Builder
+  useEffect(() => {
+    async function fetchContent() {
+      const content = await builder
+        .get("page", {
+          url: window.location.pathname,
+        })
+        .promise();
+
+      setContent(content);
+      setNotFound(!content);
+
+      // if the page title is found,
+      // set the document title
+      if (content?.data.title) {
+        document.title = content.data.title;
+      }
+    }
+    fetchContent();
+  }, []);
+
+  // If no page is found, return
+  // a 404 page from your code.
+  // The following hypothetical
+  // <FourOhFour> is placeholder.
+  if (notFound && !isPreviewingInBuilder) {
+    return <div>404 Not Found</div>;
+  }
+
+  // return the page when found
+  return (
+    <>
+      {/* Render the Builder page */}
+      <BuilderComponent model="page" content={content} />
+    </>
+  );
+}
+
+export default App;
